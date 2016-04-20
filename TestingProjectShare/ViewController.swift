920ae286288a889555805c39e1a186f325bf7603
Metalse7s <Metalse7s@macs-macbook-pro.local>
2016-04-20 16:57:44 +0200

    Haitham changed some code

--- TestingProjectShare/ViewController.swift
+++ TestingProjectShare/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     //changes here 
+    
+    //changes for haitham
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
